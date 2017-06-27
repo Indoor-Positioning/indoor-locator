378539c22f781a2e3395601a09070bbd6fd36fc1
sestus <michail.milonakis@gmail.com>
2017-06-27 20:25:36 +0300

    Change websocket address to point to debian server hosted on Okeano

--- app/src/main/java/com/mooo/sestus/indoor_locator/data/RemoteFloorPlanRepository.java
+++ app/src/main/java/com/mooo/sestus/indoor_locator/data/RemoteFloorPlanRepository.java
@@ -33,7 +33,7 @@ public class RemoteFloorPlanRepository implements FloorPlanRepository {
     private LocateCallback locateCallback;
 
     private RemoteFloorPlanRepository() {
-        Request request = new Request.Builder().url("ws://192.168.1.2:8000").build();
+        Request request = new Request.Builder().url("ws://okea.mooo.com:8000").build();
         DjangoChannelListener listener = new DjangoChannelListener();
         ws = new OkHttpClient().newWebSocket(request, listener);
         gson = new Gson();
